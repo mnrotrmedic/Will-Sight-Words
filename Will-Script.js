9937bfdb2a7faedd69a746839422b65a0f17157e
Matt. Smiles <33576579+mnrotrmedic@users.noreply.github.com>
2018-12-26 20:11:17 -0600

    Add files via upload

---
+++ Will-Script.js
@@ -0,0 +1,58 @@
+
+var sightWords = ["what", "little", "for", "was", "of", "they", "said", "want", "here", "this", "help", "too", "has",
+    // "play", "look", "good", "who", "come", "does", "where", "white", "do", "are", "with"
+];
+
+
+var randomNum = Math.floor(Math.random() * sightWords.length);
+var word = sightWords[randomNum];
+
+console.log(randomNum);
+console.log(word);
+console.log(sightWords.length);
+
+
+displayWord();
+
+function displayWord () {
+    document.getElementById("word").innerHTML = word;
+}
+
+document.getElementById("right").onclick = function removeRight () {
+    sightWords.splice(randomNum, 1);
+    console.log(sightWords);
+
+}
+
+
+
+
+// startOver();
+
+// function startOver() {
+//     console.log(sightWords.length);
+//     var randomNum = Math.floor(Math.random() * sightWords.length);
+//     var word = sightWords[randomNum];
+//     document.getElementById("word").innerHTML = word;
+//     document.getElementById("rightWord").innerHTML = word + randomNum;
+// }
+
+//Random number determined by the length of the sightWords array.
+// console.log(randomNum);
+// console.log(word);
+
+//Write word to DOM
+
+//function to cycle words based on yes click & remove correct word
+// document.getElementById("right").onclick = function () {
+//     sightWords.splice(randomNum, 1);
+//     console.log(sightWords.length);
+//     // startOver();
+//     console.log(sightWords);
+
+// }
+
+
+//function for no click that leaves the word in the array, increments the misses
+//maybe have word written to a separate div that can be displayed at the end of the game
+
